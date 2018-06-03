7fcb01fff65f7537d9001b16ca6bb9eeb1ffa8a0
Carlos Pascual <cpascual@cells.es>
2018-06-04 01:48:26 +0200

    Add examples for plotdataitem and trendset

---
+++ taurus_pyqtgraph/examples/taurusplotdataitem.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+"""Example on using a tpg.TaurusPlotDataItem on a pure pyqtgraph plot"""
+
+if __name__ == '__main__':
+    import sys
+    import numpy
+    from taurus.qt.qtgui.application import TaurusApplication
+    from taurus.qt.qtgui.tpg import TaurusPlotDataItem
+    import pyqtgraph as pg
+
+    app = TaurusApplication()
+
+    # a standard pyqtgraph plot_item
+    w = pg.PlotWidget()
+
+    #add legend to the plot, for that we have to give a name to plot items
+    w.addLegend()
+
+    # add a regular data item (non-taurus)
+    c1 = pg.PlotDataItem(name='pg item', pen='b', fillLevel=0, brush='c')
+    c1.setData(numpy.linspace(0, 2, 250))
+    w.addItem(c1)
+
+    # add a taurus data item
+    c2 = TaurusPlotDataItem(name='taurus item', pen='r', symbol='o')
+    c2.setModel('eval:Quantity(rand(256),"m")')
+    w.addItem(c2)
+
+    w.show()
+
+    sys.exit(app.exec_())
---
+++ taurus_pyqtgraph/examples/taurustrendset.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+
+
+"""Example on using a tpg.TaurusTrendSet and some related tools
+on a pure pyqtgraph plot"""
+
+if __name__ == '__main__':
+    import sys
+    from taurus.qt.qtgui.application import TaurusApplication
+    from taurus.qt.qtgui.tpg import (TaurusTrendSet, DateAxisItem,
+                                     XAutoPanTool, ForcedReadTool)
+    import pyqtgraph as pg
+
+    from taurus.core.taurusmanager import TaurusManager
+
+    taurusM = TaurusManager()
+    taurusM.changeDefaultPollingPeriod(1000)  # ms
+
+    app = TaurusApplication()
+
+    # Add a date-time X axis
+    axis = DateAxisItem(orientation='bottom')
+    w = pg.PlotWidget()
+    axis.attachToPlotItem(w.getPlotItem())
+
+    # Add the auto-pan ("oscilloscope mode") tool
+    autopan = XAutoPanTool()
+    autopan.attachToPlotItem(w.getPlotItem())
+
+    # Add Forced-read tool
+    fr = ForcedReadTool(w, period=1000)
+    fr.attachToPlotItem(w.getPlotItem())
+
+    # add legend the legend tool
+    w.addLegend()
+
+    # adding a taurus data item...
+    c2 = TaurusTrendSet(name='foo')
+    c2.setModel('eval:rand(2)')
+    w.addItem(c2)
+
+    w.show()
+
+    sys.exit(app.exec_())
