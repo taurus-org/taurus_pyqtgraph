e8511aa40eda305af68a0aff8858b4215ce04f7d
Carlos Pascual <cpascual@cells.es>
2020-05-11 13:02:55 +0200

    Add MaxBufferTool
    Add a Tool to provide a GUI control of the buffer size
    of trends

--- taurus_pyqtgraph/__init__.py
+++ taurus_pyqtgraph/__init__.py
@@ -35,6 +35,7 @@ from .plot import TaurusPlot
 from .trend import TaurusTrend
 from .legendtool import PlotLegendTool
 from .forcedreadtool import ForcedReadTool
+from .maxbuffertool import MaxBufferTool
 from .taurusimageitem import TaurusImageItem
 from .taurusplotdataitem import TaurusPlotDataItem
 from .taurustrendset import TaurusTrendSet
---
+++ taurus_pyqtgraph/maxbuffertool.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+#############################################################################
+##
+# This file is part of Taurus
+##
+# http://taurus-scada.org
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Taurus is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Taurus is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Taurus.  If not, see <http://www.gnu.org/licenses/>.
+##
+#############################################################################
+__all__ = ["MaxBufferTool"]
+
+from taurus.external.qt import QtGui, QtCore
+from taurus.qt.qtcore.configuration.configuration import BaseConfigurableClass
+
+
+class MaxBufferTool(QtGui.QWidgetAction, BaseConfigurableClass):
+    """
+    This tool provides a menu option to control the "Maximum buffer" of
+    Plot data items that implement a `setBufferSize` method
+    (see, e.g. :meth:`TaurusTrendSet.setBufferSize`).
+
+    This tool inserts an action with a spinbox and emits a `valueChanged`
+    signal whenever the value is changed.
+
+    The connection between the data items and this tool can be done manually
+    (by connecting to the `valueChanged` signal or automatically, if
+    :meth:`autoconnect()` is `True` (default). The autoconnection feature works
+    by discovering the compliant data items that are associated to the
+    plot_item.
+
+    This tool is implemented as an Action, and provides a method to attach it
+    to a :class:`pyqtgraph.PlotItem`
+    """
+
+    valueChanged = QtCore.pyqtSignal(int)
+
+    def __init__(
+        self,
+        parent=None,
+        buffer_size=65536,
+        text="Buffer size",
+        autoconnect=True
+    ):
+        BaseConfigurableClass.__init__(self)
+        QtGui.QWidgetAction.__init__(self, parent)
+
+        # defining the widget
+        self._w = QtGui.QWidget()
+        self._w.setLayout(QtGui.QHBoxLayout())
+        self._w.setToolTip("Maximum number number of points for each trend")
+        self._label = QtGui.QLabel(text)
+        self._w.layout().addWidget(self._label)
+        self._sb = QtGui.QSpinBox()
+        self._w.layout().addWidget(self._sb)
+        self._sb.setRange(1, 2**30)  # 1 giga-points
+        self._sb.setValue(buffer_size)
+        self._sb.setSingleStep(100)
+        self._sb.setSuffix(" points")
+        self._autoconnect = autoconnect
+
+        self.setDefaultWidget(self._w)
+
+        # register config properties
+        self.registerConfigProperty(
+            self.bufferSize,
+            self.setBufferSize,
+            "buffer_size"
+        )
+        self.registerConfigProperty(
+            self.autoconnect, self.setAutoconnect, "autoconnect"
+        )
+
+        # internal conections
+        self._sb.valueChanged[int].connect(self._onValueChanged)
+
+    def _onValueChanged(self, buffer_size):
+        """emit valueChanged and update all associated trendsets (if
+        self.autoconnect=True
+        """
+        self.valueChanged.emit(buffer_size)
+        if self.autoconnect() and self.plot_item is not None:
+            for item in self.plot_item.listDataItems():
+                if hasattr(item, "setBufferSize"):
+                    item.setBufferSize(buffer_size)
+
+    def attachToPlotItem(self, plot_item):
+        """Use this method to add this tool to a plot
+
+        :param plot_item: (PlotItem)
+        """
+        menu = plot_item.getViewBox().menu
+        menu.addAction(self)
+        self.plot_item = plot_item
+        # force an update of buffer_size for connected trendsets
+        self._onValueChanged(self.bufferSize())
+
+    def autoconnect(self):
+        """Returns autoconnect state
+
+        :return: (bool)
+        """
+        return self._autoconnect
+
+    def setAutoconnect(self, autoconnect):
+        """Set autoconnect state. If True, the tool will autodetect trendsets
+        associated to the plot item and will call setBufferSize
+        on each of them for each change. If False, it will only emit a
+        valueChanged signal and only those connected to it will be notified
+        of changes
+
+        :param autoconnect: (bool)
+        """
+        self._autoconnect = autoconnect
+
+    def bufferSize(self):
+        """Returns the current buffer_size value
+
+        :return: (int)
+        """
+        return self._sb.value()
+
+    def setBufferSize(self, value):
+        """Change the buffer_size value.
+
+        :param buffer_size: (int) buffer_size
+        """
+        self._sb.setValue(value)
+
+
+if __name__ == "__main__":
+    import taurus
+
+    # taurus.setLogLevel(taurus.Debug)
+    taurus.changeDefaultPollingPeriod(333)
+
+    import sys
+    from taurus.qt.qtgui.application import TaurusApplication
+    from taurus.qt.qtgui.tpg import TaurusTrendSet, DateAxisItem
+    import pyqtgraph as pg
+
+    # from taurus.qt.qtgui.tpg import MaxBufferTool
+
+    app = TaurusApplication()
+
+    w = pg.PlotWidget()
+
+    axis = DateAxisItem(orientation="bottom")
+    w = pg.PlotWidget()
+    axis.attachToPlotItem(w.getPlotItem())
+
+    # test adding the curve before the tool
+    ts1 = TaurusTrendSet(name="before", symbol="o")
+    ts1.setModel("eval:rand()+1")
+
+    w.addItem(ts1)
+
+    fr = MaxBufferTool(w, buffer_size=16)
+    fr.attachToPlotItem(w.getPlotItem())
+
+    # test adding the curve after the tool
+    ts2 = TaurusTrendSet(name="after", symbol="+")
+    ts2.setModel("eval:rand()")
+
+    w.addItem(ts2)
+
+    w.show()
+
+    ret = app.exec_()
+
+    import pprint
+
+    pprint.pprint(fr.createConfig())
+
+    sys.exit(ret)
