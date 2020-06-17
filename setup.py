6612a46101f0c92b5d866ca31e4943f610b503a0
Carlos Pascual <cpascual@cells.es>
2020-06-17 20:31:24 +0200

    Bump version 0.3.4-alpha to 0.3.4

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ classifiers = [
 
 setup(
     name="taurus_pyqtgraph",
-    version="0.3.4-alpha",
+    version="0.3.4",
     description=description,
     long_description=long_description,
     author=author,
--- taurus_pyqtgraph/__init__.py
+++ taurus_pyqtgraph/__init__.py
@@ -54,4 +54,4 @@ from .curveproperties import (
 
 
 # Do not modify the __version__ manually. To be modified by bumpversion
-__version__ = "0.3.4-alpha"
+__version__ = "0.3.4"
