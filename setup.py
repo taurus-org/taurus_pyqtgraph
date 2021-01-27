72e7b8e7a5265a37a562b10dc56271f84e3821d3
Carlos Pascual <cpascual@cells.es>
2021-01-27 18:57:31 +0100

    Bump version 0.4.10-alpha to 0.4.10

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ classifiers = [
 
 setup(
     name="taurus_pyqtgraph",
-    version="0.4.10-alpha",
+    version="0.4.10",
     description=description,
     long_description=long_description,
     author=author,
